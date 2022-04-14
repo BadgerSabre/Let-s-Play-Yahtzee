39c6a1e49480aa4256020f5568a3e164b0c7f9de
Brandon Dega <brandondega@gmail.com>
2022-04-14 10:17:21 -0500

    added dice toggle and dynamic hand size

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-let startingHand = []
+let board = []
 let hand = []
 
 function rollDie(){
@@ -11,17 +11,31 @@ function displayDie(value){
     let face = document.createElement('img')
     face.src = `../images/Dice${value}.png`
     face.addEventListener('click', () => {
-        face.style.border = '1px solid red'
+        if(face.style.border === '1px solid red') {
+            face.style.border = ''
+            hand.pop()
+            console.log(hand)
+            // hand.splice(value, hand.length)
+        } else {
+            face.style.border = '1px solid red'
+            hand.push(value)
+            console.log(hand)
+        }
     })
     document.querySelector('#content').appendChild(face)
 }
 
-document.querySelector('#start-game').addEventListener('click', () => {
+document.querySelector('#first-roll').addEventListener('click', () => {
     document.querySelector('#content').innerHTML = ''
-    startingHand = []
+    board = []
+    hand = []
     for(let i = 0; i < 5; i++){
         let value = rollDie()
-        startingHand.push(value)
+        board.push(value)
     }
-    console.log(startingHand)
+    console.log(board)
+})
+
+document.querySelector('#roll-again').addEventListener('click', ()=>{
+    let availableDice = hand.length - 5
 })
