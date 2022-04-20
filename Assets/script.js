4bf52711229997cbda02ca7f123a9620f3584ac9
Brandon Dega <brandondega@gmail.com>
2022-04-19 21:26:37 -0500

    forgot to uncomment rollcount++

--- Assets/script.js
+++ Assets/script.js
@@ -124,7 +124,7 @@ document.querySelector('#roll-again').addEventListener('click', ()=>{
         board.push(value)
     }
     console.log(board)
-    // rollCount++
+    rollCount++
     if(rollCount > 1){
         document.querySelector('#roll-again').disabled = true
     }
